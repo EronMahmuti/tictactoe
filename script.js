73229e68c5c65260691dee5439e52c43683549b2
Eron <erooonmahmuti@gmail.com>
2021-08-02 01:00:34 +0200

    javascript badge

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ console.log('welcome to my tictactoe game ');
 console.log('yeahhh!')
 console.log("hello this project should be finished faster ")
 console.log("start game when the page loads");
+console.log("how o put javascript badge on github when making projects");
