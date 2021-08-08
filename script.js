9df8bc6b3c14b69a268f431ac2227cb6757ff70c
Eron <erooonmahmuti@gmail.com>
2021-08-08 21:08:29 +0200

    last of commits

--- script.js
+++ script.js
@@ -6,3 +6,4 @@ console.log("hello this project should be finished faster ")
 console.log("start game when the page loads");
 console.log("how o put javascript badge on github when making projects");
 console.log("hlast commit before going in vacation and then come back stronger");
+console.log("hlast commit before going in vacation and start as soon as you can , if button is clicked show this");
