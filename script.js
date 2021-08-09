2eaf09fff2c0dc86255e620ef94d1786d713fd22
Eron <erooonmahmuti@gmail.com>
2021-08-09 18:26:25 +0200

    last of commits with url init

--- script.js
+++ script.js
@@ -7,3 +7,4 @@ console.log("start game when the page loads");
 console.log("how o put javascript badge on github when making projects");
 console.log("hlast commit before going in vacation and then come back stronger");
 console.log("hlast commit before going in vacation and start as soon as you can , if button is clicked show this");
+console.log("https://www.youtube.com/watch?v=GTWrWM1UsnA");
